a71aa9cb88aa68115de816187c79b4e55e503c28
verdi4na <127745074+verdi4na@users.noreply.github.com>
2023-03-14 11:15:59 +0100

    Add files via upload

---
+++ lol.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int eta = 5;
+printf("quanti hanni hai\n");
+scanf("%d", &eta);
+
+    if (eta < 18)
+    {
+        printf("la condizione è verificata\n");
+    }
+    else
+    {
+        printf("la condizione non è verificata\n");
+    }
+}
